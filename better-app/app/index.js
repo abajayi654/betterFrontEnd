dd9454329407996964ff001c0d9584d770a569c4
Abraham <abajayi654@gmail.com>
2023-07-25 02:16:27 +0100

    v1

--- better-app/app/index.js
+++ better-app/app/index.js
@@ -1,30 +1,78 @@
-import { StyleSheet, Text, View, Image } from "react-native";
+import { StyleSheet, Text, View, Image, FlatList, Modal } from "react-native";
+import { useState } from "react";
 import { useRouter, Stack } from "expo-router";
+import { MaterialCommunityIcons } from "@expo/vector-icons";
 
 import LogoTitle from "../assets/icons/LogoTitle.jsx";
+import { AppCard } from "../components/AppCard.js";
 
 export default function Page() {
+  const [modalVisble, setModalVisible] = useState(false);
+  const centres = [
+    {
+      id: 1,
+      centreName: "Sidcup Leisure Center",
+      description: `Welcome to Sidcup Leisure Center, we hope you enjoy it here`,
+      location: "2.5 miles",
+    },
+    {
+      id: 2,
+      centreName: "Erith Leisure Center",
+      description: `Welcome to ${this.centreName}, we hope you enjoy it here`,
+      location: "2.9 miles",
+    },
+    {
+      id: 3,
+      centreName: "Parkwood Leisure Center",
+      description: `Welcome to ${this.centreName}, we hope you enjoy it here`,
+      location: "3.2 miles",
+    },
+    {
+      id: 4,
+      centreName: "Crook Log Leisure Center",
+      description: `Welcome to ${this.centreName}, we hope you enjoy it here`,
+      location: "5.5 miles",
+    },
+  ];
   const router = useRouter();
+  const handlePress = () => setModalVisible(true);
   return (
     <View style={styles.container}>
       <Stack.Screen
         options={{
           headerTitle: (props) => <LogoTitle {...props} />,
           headerTitleAlign: "center",
+          headerLeft: () => <MaterialCommunityIcons name="menu" size={25} />,
+          headerRight: () => (
+            <MaterialCommunityIcons
+              name="face-man-profile"
+              size={25}
+              color="#000000"
+            />
+          ),
         }}
       />
-      <View style={styles.main}>
-        <Text style={styles.title}>whoa</Text>
-        <Text style={styles.subtitle}>This is the first page of your app.</Text>
-        <View
-          style={{
-            width: 50,
-            height: 50,
-            justifyContent: "center",
-            alignItems: "center",
-          }}
-        ></View>
-      </View>
+
+      <FlatList
+        data={centres}
+        keyExtractor={(centre) => centre.id}
+        renderItem={({ item }) => (
+          <AppCard
+            mainText={item.centreName}
+            description={item.description}
+            location={item.location}
+            handlePress={handlePress}
+          />
+        )}
+      />
+      <Modal visible={modalVisble}>
+        <View style={styles.main}>
+          <Text style={styles.title}>hello</Text>
+          <Text style={styles.subtitle}>
+            This is the first page of your app.
+          </Text>
+        </View>
+      </Modal>
     </View>
   );
 }
@@ -32,8 +80,6 @@ export default function Page() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    alignItems: "center",
-    padding: 24,
   },
   main: {
     flex: 1,
